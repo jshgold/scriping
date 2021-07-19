8fceba4c7f9f27099c7d0bd7cda131dff60d5d1c
jshgold <jshgold@naver.com>
2021-07-19 19:11:23 +0900

    Update stu_selenium_travel.py

--- stu_selenium_travel.py
+++ stu_selenium_travel.py
@@ -16,6 +16,10 @@ browser.find_elements_by_class_name("select_code__d6PLz")[1].click()
 browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[10]/div[1]/div/input').send_keys("제주")
 time.sleep(1)
 browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[10]/div[2]/section/div/a/div[1]/i[1]').click()
+browser.find_element_by_link_text("항공권 검색").click()
+
+
+
 
 
 
