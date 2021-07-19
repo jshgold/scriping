cf4a823a78303ca07f9d66abd97628d6f831f61f
jshgold <jshgold@naver.com>
2021-07-19 19:17:27 +0900

    Update stu_selenium_travel.py

--- stu_selenium_travel.py
+++ stu_selenium_travel.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
 import time
 
-
+#사이트 열기
 browser = webdriver.Chrome()
 browser.maximize_window()
 url="https://beta-flight.naver.com/"
 browser.get(url)
-
+#날짜 설정
 browser.find_elements_by_class_name("searchBox_option__2CEVQ.select_Date__1aF7Y")[0].click()
 time.sleep(1)
 browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[10]/div[2]/div[1]/div[2]/div/div[2]/table/tbody/tr[5]/td[3]/button/b').click()
 browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[10]/div[2]/div[1]/div[2]/div/div[3]/table/tbody/tr[4]/td[4]/button/b').click()
 
+#도착지 설정
 browser.find_elements_by_class_name("select_code__d6PLz")[1].click()
 browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[10]/div[1]/div/input').send_keys("제주")
 time.sleep(1)
 browser.find_element_by_xpath('//*[@id="__next"]/div/div[1]/div[10]/div[2]/section/div/a/div[1]/i[1]').click()
-browser.find_element_by_link_text("항공권 검색").click()
+
+#검색
+browser.find_element_by_class_name("searchBox_txt__3RoCw").click()
 
 
 
