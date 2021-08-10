333f84643e12e5a3ecec634e844c329e01321f97
jshgold <jshgold@naver.com>
2021-08-10 19:50:52 +0900

    tourlist crolling
    crolling

--- stu_requests_movies.py
+++ stu_requests_movies.py
@@ -18,9 +18,9 @@ soup=BeautifulSoup(res.text,"lxml")
 movies = soup.find_all("div",attrs={"class":"Vpfmgd"})
 print(len(movies))
 
-#with open("movie.html","w",encoding="utf8") as f:
-#    f.write(soup.prettify())
+with open("movie.html","w",encoding="utf8") as f:
+    f.write(soup.prettify())
 
-for movie in movies:
-    title = movie.find("div",attrs={"class":"WsMG1c nnK0zc"}).get_text()
-    print(title)
+#for movie in movies:
+#    title = movie.find("div",attrs={"class":"WsMG1c nnK0zc"}).get_text()
+#    print(title)
---
+++ tourscript.py
@@ -0,0 +1,109 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
+import re
+import time
+
+#count=0
+#url ="https://www.mcst.go.kr/kor/s_culture/tour/tourList.jsp?pSeq=&pDetailSeq=&pMenuCd=0530000000&pCurrentPage=1&pOrder=&pRegYear=2021&pRegMonth=01&pSido=&pSearchType=TITLE&pSearchWord="
+#
+#res = requests.get(url)
+#res.raise_for_status()
+#
+#soup = BeautifulSoup(res.text,'lxml')
+#
+#numbers = soup.find("div",attrs={"class":"paging m"})
+#print(numbers)
+#if numbers != None:
+#    numbers=numbers.get_text()
+#    
+#    for number in numbers:
+#        if re.search(r'[0-9]',number):
+#            count+=1
+#    print(count)
+
+
+#lst=[]
+#for i in range(5):
+#    num=i+1
+#
+#    url="https://www.mcst.go.kr/kor/s_culture/tour/tourList.jsp?pCurrentPage={}&pRegYear=2021&pRegMonth=06&pSearchType=&pSearchWord=&pSeq=&pDetailSeq=&pOrder=&pSido=&pMCurrentPage=".format(num)
+#
+#    res = requests.get(url)
+#    res.raise_for_status()
+#    soup = BeautifulSoup(res.text,'lxml')
+#
+#    tourlists = soup.find("ul",attrs={"class":"mediaWrap"}).find_all("img")
+#
+#    print(tourlists)
+#
+#    for tourlist in tourlists:
+#        image_url = tourlist['src']
+#        tourlist_name = tourlist['alt']
+#        if tourlist_name not in lst:
+#            lst.append(tourlist_name)
+#            print(tourlist_name)
+#
+#            if image_url.startswith("/"):
+#                image_url = "https://www.mcst.go.kr"+image_url
+#
+#
+#            image_res = requests.get(image_url)
+#            image_res.raise_for_status()
+#
+#            with open("{}.jpg".format(tourlist_name),"wb") as f:
+#                f.write(image_res.content)
+    
+
+    
+lst=[]
+for ye in range(2010,2022):
+    for mo in range(1,13):
+        counts=0
+        mo=str(mo)
+        if re.match(r'[0-9]',mo):
+            mo='0'+mo
+        url ="https://www.mcst.go.kr/kor/s_culture/tour/tourList.jsp?pSeq=&pDetailSeq=&pMenuCd=0530000000&pCurrentPage=1&pOrder=&pRegYear={year}&pRegMonth={month}&pSido=&pSearchType=TITLE&pSearchWord=".format(year=ye,month=mo)
+        time.sleep(2)
+        res = requests.get(url)
+        res.raise_for_status()
+
+        soup = BeautifulSoup(res.text,'lxml')
+
+        numbers = soup.find("div",attrs={"class":"paging m"})
+        if numbers != None:
+            numbers=numbers.get_text()
+            for number in numbers:
+                if re.search(r'[0-9]',number):
+                    counts+=1
+        
+        for count in range(counts):
+            pa = count+1
+            url = "https://www.mcst.go.kr/kor/s_culture/tour/tourList.jsp?pSeq=&pDetailSeq=&pMenuCd=0530000000&pCurrentPage={page}&pOrder=&pRegYear={year}&pRegMonth={month}&pSido=&pSearchType=TITLE&pSearchWord=".format(year=ye,month=mo,page=pa)
+            res = requests.get(url)
+            res.raise_for_status()
+            soup = BeautifulSoup(res.text,'lxml')
+
+            tourlists = soup.find("ul",attrs={"class":"mediaWrap"}).find_all("img")
+
+            print(tourlists)
+
+            for tourlist in tourlists:
+                image_url = tourlist['src']
+                tourlist_name = tourlist['alt']
+                if tourlist_name not in lst:
+                    lst.append(tourlist_name)
+                    print(tourlist_name)
+
+                    if image_url.startswith("/"):
+                        image_url = "https://www.mcst.go.kr"+image_url
+
+
+                    image_res = requests.get(image_url)
+                    image_res.raise_for_status()
+
+                    with open("{}{}.jpg".format(mo,tourlist_name),"wb") as f:
+                        f.write(image_res.content)
+
+        
+            
